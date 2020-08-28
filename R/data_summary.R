fe7d19b8b06a6b69df178e57bbcf80eb19f297d2
EltonJ <elton.pdsj@gmail.com>
2020-08-28 17:22:22 -0300

    Codigo da funcao com documentacao adicionada

--- R/data_summary.R
+++ R/data_summary.R
@@ -1,16 +1,17 @@
-
-#' Resumo numérico de colunas
+#' Sumario de Colunas Numericas
+#' Gera um sumario das colunas numericas de um data.frame
 #'
-#' Gera sumários específicos de colunas numéricas de um data.frames 
-#'  
-#' @param x Um data frame. Colunas não-numéricas são removidas
-#' @param na.rm Parâmetro lógico para determinar se os valores ausentes devem ser removidos
-#' 
-#' @import purrr
+#' @param x Um data frame com colunas numericas
+#' @param na.rm Logico, indicando se NAs devem ser removidos
 #' @import dplyr
+#' @import purrr
 #' @importFrom tidyr gather
+#' @export
+#' @examples
+#' data_summary(iris)
+#' data_summary(airquality, na.rm = FALSE)
 #' 
-#' #' @return Essa função retorna um \code{data.frame} com as colunas:
+#' @return Essa funcao retorna um \code{data.frame} com as colunas: 
 #' \itemize{
 #'  \item ID
 #'  \item min
@@ -18,21 +19,13 @@
 #'  \item sd
 #'  \item max
 #' }
-#' 
-#' @author Elton Júnior <elton.pdsj@gmail.com>
-#'
+#' @author Elton J (elton.pdsj@gmail.com)
 #' @seealso \link[base]{summary}
-#'
-#' @export
-#'
-#' @examples
-#' data_summary(iris)
-#' data_summary(airquality, na.rm = FALSE)
-
 data_summary <- function(x, na.rm = TRUE){
   
-  num_data <- dplyr::select_if(x, .predicate = is.numeric) 
+  num_data <- select_if(x, .predicate = is.numeric) 
   
-  purrr::map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
+  map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
   
 }
+
